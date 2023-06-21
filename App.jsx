fa28f8df09acffb9b92eba45ab87b29f1f4c7628
Nikhil harchand <122405289+nikhilharchand@users.noreply.github.com>
2023-06-21 23:39:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import "./App.css";
+import Todo from "./todo";
+import styles from "./TodoApp.module.css";
+
+function App() {
+    const [todos, setTodos] = useState([
+        { id: 1, title: "Go for morning run", completed: true },
+        { id: 2, title: "Teach react to students", completed: false },
+        { id: 3, title: "Fix bugs of codprog website", completed: false },
+    ]);
+
+    function deleteTodo(id) {
+        // setTodos((prevTodos) => {
+        //     return prevTodos.filter((todo) => (todo.id !== id) );
+        // });
+        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    }
+
+    function toggleCompleted(id) {
+        // const newArray = todos.map((todo) => {
+        //     if (todo.id === id) {
+        //         return { ...todo, completed: !todo.completed };
+        //     } else {
+        //         return todo;
+        //     }
+        // });
+        // setTodos(newArray);
+
+        setTodos((prevTodos) => {
+            return prevTodos.map((todo) => {
+                if (todo.id === id) {
+                    return { ...todo, completed: !todo.completed };
+                } else {
+                    return todo;
+                }
+            });
+        });
+    }
+
+    function addTodo(newTodo) {
+        setTodos((prevTodos) => [...prevTodos, newTodo]);
+    }
+
+    function clearAll() {
+        setTodos([]);
+    }
+    return (
+        <div className={styles.todoApp}>
+            <h1>Todo App</h1>
+            <AddTodoForm addTodo={addTodo} />
+            {/* {todos.map((todo) => {
+                return (
+                    <Todo
+                        key={todo.id}
+                        title={todo.title}
+                        completed={todo.completed}
+                        id={todo.id}
+                        deleteTodo={deleteTodo}
+                        toggleCompleted={toggleCompleted}
+                    />
+                );
+            })} */}
+
+            {todos.map((todo) => {
+                return (
+                    <Todo
+                        key={todo.id}
+                        {...todo}
+                        deleteTodo={deleteTodo}
+                        toggleCompleted={toggleCompleted}
+                    />
+                );
+            })}
+            {todos.length > 0 && <button onClick={clearAll}>Clear All</button>}
+        </div>
+    );
+}
+
+export default App;
---
+++ todo.jsx
@@ -0,0 +1,27 @@
+import styles from "./Todo.module.css";
+function Todo({ title, completed, deleteTodo, toggleCompleted, id }) {
+    return (
+        <div className={styles.todo}>
+            <h2 className={completed ? styles.completed : null}>{title}</h2>
+            <div className={styles.buttonWrapper}>
+                <button
+                    className="delete"
+                    onClick={() => {
+                        deleteTodo(id);
+                    }}
+                >
+                    Delete
+                </button>
+                <button
+                    className="done"
+                    onClick={() => {
+                        toggleCompleted(id);
+                    }}
+                >
+                    {completed ? "Undone" : "Done"}
+                </button>
+            </div>
+        </div>
+    );
+}
+export default Todo;
